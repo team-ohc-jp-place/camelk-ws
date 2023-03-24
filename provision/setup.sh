3ab6fdf1066c70a9f140b5617e4f8a60db67de91
kamorisan <kamori@redhat.com>
2023-03-25 01:09:53 +0900

    updates

--- provision/setup.sh
+++ provision/setup.sh
@@ -37,7 +37,30 @@ while [ true ] ; do
   sleep 10
 done
 
+# Etherpad
+oc new-project gpte-etherpad --display-name "OpenTLC Shared Etherpad"
+oc new-app --template=postgresql-persistent \
+  --param POSTGRESQL_USER=ether \
+  --param POSTGRESQL_PASSWORD=ether \
+  --param POSTGRESQL_DATABASE=etherpad \
+  --param POSTGRESQL_VERSION=10 \
+  --param VOLUME_CAPACITY=10Gi \
+  --labels=app=etherpad_db
+
+sleep 15
+
+oc new-app -f https://raw.githubusercontent.com/wkulhanek/docker-openshift-etherpad/master/etherpad-template.yaml \
+  -p DB_TYPE=postgres \
+  -p DB_HOST=postgresql \
+  -p DB_PORT=5432 \
+  -p DB_DATABASE=etherpad \
+  -p DB_USER=ether \
+  -p DB_PASS=ether \
+  -p ETHERPAD_IMAGE=quay.io/wkulhanek/etherpad:1.8.4 \
+  -p ADMIN_PASSWORD=secret
+
 # Devspaces Create Workspaces
+oc project devspaces
 while [ 1 ]; do
   CSV=$(oc -n openshift-operators get subscription devspaces -o=jsonpath='{.status.currentCSV}')
   STAT=$(oc -n openshift-operators get ClusterServiceVersion $CSV -o=jsonpath='{.status.phase}')
@@ -217,12 +240,3 @@ for m in $(eval echo "{1..$USER_COUNT}"); do
 
 done
 
-# Etherpad
-oc new-project gpte-etherpad --display-name "OpenTLC Shared Etherpad"
-oc new-app --template=postgresql-persistent \
-  --param POSTGRESQL_USER=ether \
-  --param POSTGRESQL_PASSWORD=ether \
-  --param POSTGRESQL_DATABASE=etherpad \
-  --param POSTGRESQL_VERSION=10 \
-  --param VOLUME_CAPACITY=10Gi \
-  --labels=app=etherpad_db
